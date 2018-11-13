3665abe8d429e870517d300161819cebfc359adb
BlanckeKenny <kenny.blancke@icloud.com>
2018-11-13 20:03:06 +0100

    05_BookRepository

---
+++ BookService.WebApi/Repositories/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookService.WebApi.DTO;
+using BookService.WebApi.Models;
+
+namespace BookService.WebApi.Repositories
+{
+    public class BookRepository
+    {
+        private BookServiceContext db;
+
+        public BookRepository(BookServiceContext context)
+        {
+            db = context;
+        }
+
+        public List<Book> List()
+        {
+            return db.Books.ToList();
+        }
+
+        public List<BookBasic> ListbBasic()
+        {
+            // returns a list of BookBasic DTO-Items (Id and Title only)
+            return db.Books.Select(b => new BookBasic
+            {
+                Id = b.Id,
+                Title = b.Title
+            }).ToList();
+        }
+
+
+    }
+}
--- BookService.WebApi/Startup.cs
+++ BookService.WebApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookService.WebApi.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,7 @@ namespace BookService.WebApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<BookServiceContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("Bookservice")));
+            services.AddScoped<BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
