d0b8468bf66afed5ac4a4a431ee3b3c9339c7aaa
BlanckeKenny <kenny.blancke@icloud.com>
2018-11-18 12:16:27 +0100

    BookDetailsPage completed

--- Bookservice.MVC/Controllers/BooksController.cs
+++ Bookservice.MVC/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Bookservice.MVC.Models;
 using BookServiceLib.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -12,31 +13,36 @@ namespace Bookservice.MVC.Controllers
     public class BooksController : Controller
     {
 
-        private const string BaseUri = "https://localhost:44375/api/books";
+        private const string BaseBookUri = "https://localhost:44375/api/books";
 
 
         public IActionResult Index()
         {
-            string BookUri = $"{BaseUri}/basic";
-            return View(GetApiResult<List<BookBasic>>(BookUri));
+            var basicBookUri = $"{BaseBookUri}/basic";
+            return View(GetApiResult<List<BookBasic>>(basicBookUri));
         }
-
-
 
         public T GetApiResult<T>(string uri)
         {
             using (HttpClient httpClient = new HttpClient())
             {
                 Task<string> response = httpClient.GetStringAsync(uri);
-                return Task.Factory.StartNew(
-                    () => JsonConvert.DeserializeObject<T>(response.Result)
-                ).Result;
+                return Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(response.Result)).Result;
             }
         }
 
-        public IActionResult Detail()
+        public IActionResult Detail(int id)
         {
-            return View();
+            var geekJokesUri = "https://geek-jokes.sameerkumar.website/api";
+            var ipsumUri = "https://baconipsum.com/api/?type=meat-andfiller&paras=2&format=text";
+            var bookUri = $"{BaseBookUri}/detail/{id}";
+
+            return View( new BookDetailExtraViewModel
+            {
+                BookDetail = GetApiResult<BookDetail>(bookUri),
+                AuthorJoke = GetApiResult<string>(geekJokesUri),
+                BookSummary = new HttpClient().GetStringAsync(ipsumUri).Result
+            });
         }
     }
 }
---
+++ Bookservice.MVC/Models/BookDetailExtraViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookServiceLib.DTO;
+
+namespace Bookservice.MVC.Models
+{
+    public class BookDetailExtraViewModel
+    {
+        public BookDetail BookDetail { get; set; }
+        public string AuthorJoke { get; set; }
+        public string BookSummary { get; set; }
+    }
+}
